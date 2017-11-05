c90ca17e1d5da05eb6943c0e31efd54d81eb23b9
Chase Moskal <lordbrimshaw@gmail.com>
2017-11-05 15:45:59 -0800

    update readme

--- source/example.ts
+++ source/example.ts
@@ -1,11 +1,11 @@
 
 import {FileLogger} from "./index"
 
-const logger = new FileLogger({level: "silly", logfile: "example.log"})
+const logger = new FileLogger({logfile: "example.log", level: "silly"})
 
-logger.error(new Error("example error occurred"))
-logger.warn("heed my warning")
-logger.info("example info")
-logger.verbose("sample verbose")
-logger.debug("example debug")
-logger.silly("sample silly")
+logger.error(new Error("an error occurred"))
+logger.warn("heed my warning!")
+logger.info("something informative")
+logger.verbose("explaining what's happening verbosely")
+logger.debug("information to help diagnose bugs")
+logger.silly("obscenely fine-grained details")
